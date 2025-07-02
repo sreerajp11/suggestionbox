c8310398648a54fdff78ccce081228e5b585fd1f
Sree Raj <sree.raj@anca.com>
2025-07-02 18:42:03 +0530

    html for front end

---
+++ src/test/java/com/amti/suggestionbox/SuggestionboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amti.suggestionbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
